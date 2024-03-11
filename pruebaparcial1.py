6148188cb92fdb585c19bf38352cb1f16be9e482
juanf1021 <82191280+juanf1021@users.noreply.github.com>
2024-03-10 22:43:46 -0500

    se cambio el codigo original para usar la funcion editableGraph de netgraph que ya permite modificar eliminar y agregar sin funciones extra, tambien se agrego codigo para postrar el costo de los arcos

--- pruebaparcial1.py
+++ pruebaparcial1.py
@@ -1,69 +1,61 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
-import netgraph as ng
+import matplotlib.pyplot as plt  
+import networkx as nx 
+from netgraph import EditableGraph 
+import numpy as np  
 
-def update_graph():
-    global I
-    plt.clf()  # Borra la figura actual
-    I = ng.InteractiveGraph(G,
-                            node_positions=pos,
-                            node_labels=True,
-                            node_label_bbox=dict(fc="lightgreen", ec="black", boxstyle="square", lw=3),
-                            node_size=12)
-    plt.ion()
-    plt.show()
+# Crea un grafo dirigido
+g = nx.DiGraph()
 
+# Lista de tuplas de aristas con sus atributos (pesos)
+edges_list = [
+    (1, 2, {'weight': 1}),
+    (1, 4, {'weight': 1.1}),
+    (2, 5, {'weight': 0.5}),
+    (4, 5, {'weight': 1.3}),
+    (2, 3, {'weight': 0.8})
+]
 
-def add_new_graph(event):
-    # Esta función se activará cuando se presione una tecla
-    if event.key == 'a':
-        # Agrega nodos con atributos al grafo existente
-        G.add_node(6, nombre='Tarea 6', duracion=3, costo=15, prerrequisitos=[], postrequisitos=[])
-        G.add_node(7, nombre='Tarea 7', duracion=6, costo=25, prerrequisitos=[6], postrequisitos=[])
-        
-        # Agrega aristas al grafo existente según necesites
-        G.add_edge(6, 7)
-        
-        # Actualiza y redibuja el gráfico
-        update_graph()
-# Crear un grafo dirigido vacío
-G = nx.DiGraph()
+# Añade las aristas al grafo
+g.add_edges_from(edges_list)
 
-# Añadir nodos con atributos
-G.add_node(1, nombre='Tarea 1', duracion=5, costo=20, prerrequisitos=[], postrequisitos=[2,4])
-G.add_node(2, nombre='Tarea 2', duracion=4, costo=20, prerrequisitos=[1], postrequisitos=[3,5])
-G.add_node(3, nombre='Tarea 3', duracion=10, costo=50, prerrequisitos=[2], postrequisitos=[5])
-G.add_node(4, nombre='Tarea 4', duracion=4, costo=20, prerrequisitos=[1], postrequisitos=[5])
-G.add_node(5, nombre='Tarea 5', duracion=5, costo=30, prerrequisitos=[2, 3, 4], postrequisitos=[])
+# Crea un diccionario para almacenar los pesos de las aristas
+edges_weight = {}
+for i in range(len(edges_list)):
+    edges_weight[(edges_list[i][0], edges_list[i][1])] = edges_list[i][2]['weight']
 
-# Añadir aristas al grafo según la imagen
-G.add_edge(1, 2)
-G.add_edge(1, 4)
-G.add_edge(2, 5)
-G.add_edge(4, 5)
-G.add_edge(2, 3)
+# Diccionario para almacenar los colores de las aristas
+edge_color = dict()
+for ii, (source, target) in enumerate(g.edges):
+    edge_color[(source, target)] = 'tab:gray' if ii % 2 else 'tab:orange'
 
-# Definir posiciones iniciales de los nodos
+# Diccionario para almacenar los colores de los nodos y define los atributos de los nodos
+node_color = dict()
+g.add_nodes_from([
+     (1, {"nombre": 'Tarea 1', "duracion": 4, "costo": 20, "prerrequisitos": [1], "postrequisitos": [3, 5]}),
+     (2, {"nombre": 'Tarea 2', "duracion": 10, "costo": 50, "prerrequisitos": [2], "postrequisitos": [5]}),
+     (3, {"nombre": 'Tarea 3', "duracion": 15, "costo": 20, "prerrequisitos": [1], "postrequisitos": [5]}),
+     (4, {"nombre": 'Tarea 4', "duracion": 15, "costo": 20, "prerrequisitos": [1], "postrequisitos": [5]}),
+     (5, {"nombre": 'Tarea 5', "duracion": 15, "costo": 20, "prerrequisitos": [1], "postrequisitos": [5]}),
+])
+
+# Define los colores de los nodos
+for node in g.nodes:
+    node_color[node] = 'tab:red' if node % 2 else 'tab:blue'
+
+# Crea la figura y los ejes para el gráfico
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Posiciones de los nodos
 pos = {1: np.array([1, 1]), 2: np.array([1, 2]), 3: np.array([1, 3]), 4: np.array([1, 4]), 5: np.array([1, 5])}
 
-# Verificar y ajustar posiciones para evitar divisiones por cero
-for node, position in pos.items():
-    if np.linalg.norm(position) == 0:
-        pos[node] = np.array([0, 0]) # Ajustar posición a un vector no nulo
-
-# Crear un gráfico interactivo con netgraph
-I = ng.InteractiveGraph(G,
-                              node_positions=pos,
-                              node_labels=True,
-                              node_label_bbox=dict(fc="lightgreen", ec="black", boxstyle="square", lw=3),
-                              node_size=12)
-
-# Registrar la función para activar cuando se presione una tecla
-plt.gcf().canvas.mpl_connect('key_press_event', add_new_graph)
-
-# Mostrar el gráfico interactivo
-plt.ion()
+# Grafica el grafo editable con sus atributos y posiciones definidos
+plot_instance = EditableGraph(g, pos=pos,
+    node_color=node_color, node_size=5, edge_labels=edges_weight,
+    node_labels=True, edge_layout_kwargs=dict(k=0.025),
+    node_label_fontdict=dict(size=20),
+    edge_color=edge_color, edge_width=2,
+    annotation_fontdict=dict(color='blue', fontsize=15), edge_label_fontdict=dict(fontweight='bold'),
+    arrows=True, ax=ax)
+    
+# Muestra el gráfico
 plt.show()
-plt.pause(10000) # Pausa para mantener la ventana abierta
-
