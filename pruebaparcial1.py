4c262aad076e24ee41e7cda64873ced729f9e53f
juanf1021 <82191280+juanf1021@users.noreply.github.com>
2024-04-16 01:47:17 -0500

    funcion encontrada para traer los nodes y edges del grafo modificado

--- pruebaparcial1.py
+++ pruebaparcial1.py
@@ -26,4 +26,27 @@ plot_instance = EditableGraph(
     annotations=annotations, annotation_fontdict = dict(color='blue', fontsize=15),
     arrows=True, ax=ax)
 
+def transform_edges(edges_dict):
+    transformed_edges = []
+    for edge, text_obj in edges_dict.items():
+        # Obtener el texto del objeto Text
+        text = text_obj.get_text()
+        # Verificar si el texto está vacío
+        if text == '':
+            # Asignar un valor predeterminado al peso o simplemente omitir la conversión
+            weight = 0.0 # Valor predeterminado
+        else:
+            # Convertir el texto a float si no está vacío
+            weight = float(text)
+        # Crear una tupla con los dos primeros elementos de la clave del diccionario y un diccionario con el peso
+        transformed_edge = (edge[0], edge[1], {'weight': weight})
+        transformed_edges.append(transformed_edge)
+    return transformed_edges
+
+
+edges = plot_instance.edge_label_artists 
+edges_l = transform_edges(edges)
+print(edges)
+print(edges_l)
+
 plt.show()
